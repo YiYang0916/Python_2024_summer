28a4df958f052872b6e1b70de4fb0da5a02f3609
YiYang0916 <shinybeing@eysi.com.tw>
2024-07-30 20:16:38 +0800

    Create ex3-7.py

---
+++ ex3-7.py
@@ -0,0 +1,16 @@
+x=7
+y='seven'
+print(type(x))
+print(type(y))
+
+x=3  # x is of type int
+x='three'  # x in now of type str
+print(type(x))
+
+x="pig"
+y='PIG'
+print(x,y)
+
+x=9
+X='nine'
+print(x)
