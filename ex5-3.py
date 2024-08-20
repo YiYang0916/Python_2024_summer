fdfe2275a03c64cda498975c80f3af0d74a5f092
YiYang0916 <shinybeing@eysi.com.tw>
2024-08-20 19:36:18 +0800

    Create ex5-3.py

---
+++ ex5-3.py
@@ -0,0 +1,15 @@
+for x in range(10):
+  print(x)
+else:
+  print("Stops at 9 not 10")
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+my_function()
+my_function()
+my_function()
+my_function()
